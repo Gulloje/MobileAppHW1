2dc6270a39d5900ec3e2590b0931d38766faec9a
JessieGullo <129686666+Gulloje@users.noreply.github.com>
2024-01-29 12:27:00 -0500

    feedback questions

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.homework1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private val upperBound = 100;
     private var score = 0;
     private var questionsAsked = 0;
+    private var message = ""
+    private var correctStreak = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,22 +25,39 @@ class MainActivity : AppCompatActivity() {
 
     fun resetGame(view: View) {
         createEquation();
+        score = 0;
+        questionsAsked = 0;
+        findViewById<TextView>(R.id.feedback).text = ""
+        findViewById<TextView>(R.id.score).text = "Score: $score/$questionsAsked"
     }
 
     fun checkCorrect(view: View) {
         val op1 = findViewById<TextView>(R.id.operand1).text.toString().toInt()
         val op2 = findViewById<TextView>(R.id.operand2).text.toString().toInt()
         val answer = findViewById<TextView>(R.id.answer).text.toString().toInt()
+
         //println(op1);
         //println(R.id.answer.toString().toInt())
         if (view.id == R.id.btnPlus && (op1 + op2 == answer) ||
             view.id == R.id.btnMinus && (op1 - op2 == answer) ||
             view.id == R.id.btnMult && (op1 * op2 == answer)) {
             score++;
-            //println("correct");
-        } else {
-            //println("incorrect");
+            correctStreak++
+            message = "Correct!"
+            findViewById<TextView>(R.id.feedback).setTextColor(Color.parseColor("#00FF00"));
 
+        } else {
+            //val myTextView = findViewById<TextView>(R.id.text_view_id)
+            //myTextView.setTextColor(Color.parseColor("#FF0000"))
+            findViewById<TextView>(R.id.feedback).setTextColor(Color.parseColor("#FF0000"));
+            correctStreak = 0
+
+
+        }
+        findViewById<TextView>(R.id.feedback).text = "$message"
+        println(message)
+        if (correctStreak == 3) {
+            Toast.makeText(this, "Streak = 3", Toast.LENGTH_SHORT).show()
         }
         questionsAsked++;
         findViewById<TextView>(R.id.score).text = "Score: $score/$questionsAsked"
@@ -52,9 +73,18 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.operand2).text = "$op2"
         var operation = getRandomNumber(1,3);
         when(operation) {
-            1 -> findViewById<TextView>(R.id.answer).text = "${op1 + op2}"
-            2 -> findViewById<TextView>(R.id.answer).text = "${op1 - op2}"
-            3 -> findViewById<TextView>(R.id.answer).text = "${op1 * op2}"
+            1 -> {
+                findViewById<TextView>(R.id.answer).text = "${op1 + op2}"
+                message = "Wrong: $op1 + $op2 = ${op1+op2}"
+            }
+            2 -> {
+                findViewById<TextView>(R.id.answer).text = "${op1 - op2}"
+                message = "Wrong: $op1 - $op2 = ${op1-op2}"
+            }
+            3 -> {
+                findViewById<TextView>(R.id.answer).text = "${op1 * op2}"
+                message = "Wrong: $op1 * $op2 = ${op1*op2}"
+            }
         }
 
     }
